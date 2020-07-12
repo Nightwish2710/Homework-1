1c0fca418bad004fe900fcb714516e38c5b600a3
Nightwish2710 <45417031+Nightwish2710@users.noreply.github.com>
2020-07-12 21:55:23 +0700

    Update

---
+++ Assets/BirdController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+	public float maxPos = 10f;
+	public float minPos = -10f;
+	public float currentPos = -10f;
+	public float speed = 5f;
+
+	private Vector2 startPos;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		startPos = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		currentPos += Time.deltaTime * speed;
+
+		if (currentPos >= maxPos)
+		{
+			currentPos = minPos;
+		}
+
+		transform.position = new Vector2(currentPos, startPos.y);
+	}
+}
